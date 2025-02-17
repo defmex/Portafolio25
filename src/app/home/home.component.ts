100e61be1c4531f79385d523d9773f953701e708
defmex <def>
2025-02-17 01:40:24 -0300

    config navbar basic of standar web

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProyectComponent } from "../proyect/proyect.component";
 import { LanguangeCodingComponent } from "../languange-coding/languange-coding.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProyectComponent, LanguangeCodingComponent],
+  imports: [ProyectComponent, LanguangeCodingComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
