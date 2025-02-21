0453af900f5be82f5e707ea6964737c1a9299d60
defmex <def>
2025-02-21 02:41:26 -0300

    add the foter and form for my contac

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProyectComponent } from "../proyect/proyect.component";
 import { LanguangeCodingComponent } from "../languange-coding/languange-coding.component";
 import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProyectComponent, LanguangeCodingComponent, NavbarComponent],
+  imports: [ProyectComponent, LanguangeCodingComponent, NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
